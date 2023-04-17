91cfee991dc6725140f791a1c2ccb9cc6cec6f70
Simon Onyszczak Hansen <simo401t@zbc.dk>
2023-04-17 12:21:25 +0200

    Add project files.

---
+++ Øvelse 3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Øvelse_3
+{
+    class MyClass
+    {
+        Random rnd = new Random();
+        public int GenerateNumber()
+        {
+            Thread.Sleep(2000);
+            return rnd.Next(-20, 121);
+
+        }
+        public void CheckTemp()
+        {
+            int count = 0;
+            while (count <3)
+            {
+                int temp = GenerateNumber();
+                Console.WriteLine(temp.ToString());
+                if (temp < 0 || temp > 100)
+                {
+                    Console.WriteLine("Temperaturen ligger ude for det ulovlige interval!!");
+                    count++;
+                }
+            }
+            Console.WriteLine("Alarm-tråd termineret!");
+
+        }
+        
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass mc = new MyClass();
+            Thread t1 = new Thread(mc.CheckTemp);
+            t1.Start();
+            Console.Read();
+        }
+    }
+}
